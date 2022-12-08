29c959ecfbd4db8c18bbed7535736850a760d595
poptsov <alexey.poptsov@mail.ru>
2022-12-08 18:15:52 +0300

    book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script. on book
+# This is a sample Python script. on book new branch
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
